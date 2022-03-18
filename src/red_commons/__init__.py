b57036e87885a05bad5acf8f74e2177c827a78f4
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-03-18 16:56:46 +0000

    add logging utils

--- src/red_commons/__init__.py
+++ src/red_commons/__init__.py
@@ -1,5 +1,5 @@
 """Common utilities used by multiple projects maintained by Cog Creators."""
+from . import logging
 
 __version__ = "1.0.0"
-
-__all__ = ()
+__all__ = ("logging",)
---
+++ src/red_commons/logging/__init__.py
@@ -0,0 +1,81 @@
+import asyncio
+import contextlib
+import logging
+
+VERBOSE = logging.DEBUG - 3
+TRACE = logging.DEBUG - 5
+
+__all__ = [
+    "RedTraceLogger",
+    "VERBOSE",
+    "TRACE",
+    "update_logger",
+    "task_callback_critical",
+    "task_callback_exception",
+    "task_callback_warning",
+    "task_callback_debug",
+    "task_callback_verbose",
+    "task_callback_trace",
+]
+
+
+class RedTraceLogger(logging.getLoggerClass()):
+    def __init__(self, name, level=logging.NOTSET):
+        super().__init__(name, level)
+
+        logging.addLevelName(VERBOSE, "VERBOSE")
+        logging.addLevelName(TRACE, "TRACE")
+
+    def verbose(self, msg, *args, **kwargs):
+        if self.isEnabledFor(VERBOSE):
+            self._log(VERBOSE, msg, args, **kwargs)
+
+    def trace(self, msg, *args, **kwargs):
+        if self.isEnabledFor(TRACE):
+            self._log(TRACE, msg, args, **kwargs)
+
+
+def update_logger():
+    if not isinstance(logging.getLoggerClass(), RedTraceLogger):
+        logging.setLoggerClass(RedTraceLogger)
+
+
+update_logger()
+
+_log = logging.getLogger("red.commons.logging")
+
+
+def task_callback_critical(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.critical("%s raised an Exception", task.get_name(), exc_info=exc)
+
+
+def task_callback_exception(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.exception("%s raised an Exception", task.get_name(), exc_info=exc)
+
+
+def task_callback_warning(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.warning("%s raised an Exception", task.get_name(), exc_info=exc)
+
+
+def task_callback_debug(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.debug("%s raised an Exception", task.get_name(), exc_info=exc)
+
+
+def task_callback_verbose(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.verbose("%s raised an Exception", task.get_name(), exc_info=exc)
+
+
+def task_callback_trace(task: asyncio.Task) -> None:
+    with contextlib.suppress(asyncio.CancelledError, asyncio.InvalidStateError):
+        if exc := task.exception():
+            _log.trace("%s raised an Exception", task.get_name(), exc_info=exc)
